57d952e71c5c6636349de0e83f1f127d367782ad
Colin <mcbolin@hotmail.com>
2024-07-05 13:00:18 +1000

    feat: route queryParams

--- src/routers/pokemonRouters.js
+++ src/routers/pokemonRouters.js
@@ -5,9 +5,27 @@ router.get("/", (request, response) => {
     response.json({message:"Router route activated!"});
 });
 
-router.get("/random", (request, response) => {
-    response.json({message: "Random Pokemon route activated!"});
+
+
+router.get("/getbyid/:pokemonNumber", (request, response) => {
+
+    let retrievedNumberFromUrl = request.params.pokemonNumber;
+
+    response.json({
+        number: retrievedNumberFromUrl
+    });
 });
+
+router.get("/random", (request, response) => {
+    console.log("Random route");
+    let queryParams = request.query;
+    response.json({
+        message: "Random Pokemon route activated!",
+        queryParams: queryParams
+    });
+});
+
+
 
 router.post("/", (request, response) => {
     response.json({message: "No POSTing here!"});
